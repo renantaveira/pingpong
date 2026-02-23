925a8eacb41c254cb63023fff47fa1597cea6675
renantaveira <renan.taveira@escola.pr.gov.br>
2026-02-23 12:13:11 -0300

    Update pingpong.js

--- pingpong.js
+++ pingpong.js
@@ -23,3 +23,22 @@
 function setup() {
   createCanvas(600, 400);
 }
+
+function draw(){
+  
+}
+
+function movimentoBolinha(){
+  circle(xBolinha,yBolinha,diametro);
+  
+  xBolinha += velocidadeXBolinha;
+  yBolinha += velocidadeYBolinha;
+  if(xBolinha + raio > width || xBolinha-raio < 0){
+    velocidadeXBolinha *= -1;
+  }
+  if(yBolinha + raio > height || yBolinha-raio < 0){
+    velocidadeYBolinha *= -1}}
+
+function raquetes(xRaquete,yRaquete){
+  rect(xRaquete,yRaquete, 10,90)
+}
